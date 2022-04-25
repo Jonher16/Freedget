ca6309195cd71911b61b41b08f00d0c66531b3fb
Jonher16 <87830930+jon-hernandez16@users.noreply.github.com>
2022-04-25 13:18:18 +0200

    Fixed add to chart function

--- src/components/Categorization.js
+++ src/components/Categorization.js
@@ -5,87 +5,121 @@ import { AiOutlineEdit } from "react-icons/ai"
 import { useState } from 'react'
 import Plot from 'react-plotly.js';
 import CategoryComp from './CategoryComp'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const Categorization = () => {
 
     const [categories, setCategories] = useState([])
-    const [showInput, setShowInput] = useState(false)
     const [category, setCategory] = useState("")
-    const [value, setValue] = useState()
+    const [tempIncome, setTempIncome] = useState()
+    const [income, setIncome] = useState()
     const [data, setData] = useState(
         {
-            values: [],
-            labels: [],
+            values: [100],
+            labels: ["No Category"],
             type: "pie",
         }
     )
-    const [sum, setSum] = useState(0)
-
-    useEffect(() => {
-    var tempdata = data
-    var tempsum = tempdata.values.reduce((a, b) => parseInt(a) + parseInt(b), 0);
-    setSum(tempsum)
-    console.log(tempsum)
-    }, [data])
-    
-
-    const toggleShowInput = () => {
-        setShowInput(true)
-    }
 
     const addToCatList = (e) => {
         e.preventDefault()
         var temp_cat = category
         setCategories([...categories, temp_cat])
-        setShowInput(false)
-        setCategory("")
-        setData(prevData=>({
+        var tempdata = data
+        setData(prevData => ({
             ...prevData,
             labels: [...prevData.labels, temp_cat],
-            values: [...prevData.values, 25]
+            values: [...prevData.values, 0]
+        }))
+        setCategory("")
+    }
+
+    const addValue = (e, cat, value) => {
+        e.preventDefault()
+        var tempvalue = parseInt(value)
+        var tempdata = data
+        var temp_income = income
+        var index = tempdata.labels.indexOf(cat)
+        tempdata.values[index] = tempvalue
+        tempdata.values.shift()
+        var tempsum = tempdata.values.reduce((a, b) => parseInt(a) + parseInt(b), 0);
+        console.log(tempsum)
+        tempdata.values.unshift(temp_income - tempsum)
+        if (temp_income - tempsum >= 0) {
+            toast(`${cat} value set to ${value}€ !`)
+            setData(prevData => ({
+                ...prevData,
+                values: [...tempdata.values],
+            }))
+        } else if (temp_income - tempsum < 0) {
+            toast("Sum of categories is greater than income :(")
+        } else {
+            toast("Error: Sum is not int")
+        }
+    }
+
+    const addIncome = (e) => {
+        e.preventDefault()
+        setIncome(tempIncome)
+        toast(`Income set to ${tempIncome}€ !`)
+        var tempdata = data
+        console.log("Tempdatavaluesbeforeshift", tempdata.values)
+        tempdata.values.shift()
+        var tempsum = tempdata.values.reduce((a, b) => parseInt(a) + parseInt(b), 0);
+        console.log("TempSum", tempsum)
+        tempdata.values.unshift(tempIncome - tempsum)
+        console.log("TempdataValues =>", tempdata.values)
+        setData(prevData => ({
+            ...prevData,
+            labels: [...prevData.labels],
+            values: [...tempdata.values]
         }))
     }
 
-    const addValue = (e, cat) => {
-        e.preventDefault()
-        var tempvalue = value
-        var tempdata = data
-        var index = tempdata.labels.indexOf(cat)
-        setData(prevData=>({
-            ...prevData,
-            values: [...prevData.values, prevData.values[index] = tempvalue]
-        }))
-    }
+    useEffect(() => {
+        console.log("Data =>", data)
+    }, [data])
+
 
     return (
 
         <div className="m-3 d-flex flex-row">
+            <ToastContainer></ToastContainer>
             <div className="w-50">
                 <h2>Categorization</h2>
-                <Form className="w-50 mt-3" onSubmit={e => addToCatList(e)}>
-                    <div className="bprder d-flex flex-row w-100">
-                        <Button className="mr-3" onClick={toggleShowInput}>Add Category</Button>
-                        {showInput ?
-                            (<Form.Control type="text" className="w-50" onChange={e => setCategory(e.target.value)} placeholder="Category name" />) : ""
-                        }
+                <Form className="w-50 mt-3" onSubmit={e => addIncome(e)}>
+                    <div className="w-100">
+                        <Form.Label>Set income</Form.Label>
+                        <Form.Control type="number" className="w-50" onChange={e => setTempIncome(e.target.value)} placeholder="Set income €" />
                     </div>
                 </Form>
-                <Card className="mt-3" style={{ width: '18rem' }}>
-                    <Card.Header>Category list</Card.Header>
-                    <ListGroup variant="flush">
-                        {categories.map((cat, i) =>
-                            <CategoryComp key={i} id={i} cat={cat} onSubmit={e => addValue(e, cat)} onChange={e => setValue(e.target.value)} />
-                        )
-                        }
-                    </ListGroup>
-                </Card>
+                {income &&
+                    <>
+                        <Form className="w-50 mt-3" onSubmit={e => addToCatList(e)}>
+                            <div className="w-100">
+                                <Form.Label>Add new category</Form.Label>
+                                <Form.Control value={category} type="text" className="w-50" onChange={e => setCategory(e.target.value)} placeholder="Category name" />
+                            </div>
+                        </Form>
+                        <Card className="mt-3" style={{ width: '18rem' }}>
+                            <Card.Header>Category list</Card.Header>
+                            <ListGroup variant="flush">
+                                {categories.map((cat, i) =>
+                                    <CategoryComp key={i} id={i} cat={cat} onSubmit={addValue} />
+                                )
+                                }
+                            </ListGroup>
+                        </Card>
+                    </>
+                }
             </div>
             <Plot
                 data={[data]}
                 layout={{
-                    width: 500, 
-                    height: 500, 
-                    title: 'Category Division', 
-                    paper_bgcolor: "#1E3D58", 
+                    width: 500,
+                    height: 500,
+                    title: 'Category Division',
+                    paper_bgcolor: "#1E3D58",
                     font: {
                         color: "#43B0F1",
                     },
--- src/components/CategoryComp.js
+++ src/components/CategoryComp.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form, ListGroup } from 'react-bootstrap'
 
 const CategoryComp = ({id, cat, onSubmit, onChange}) => {
+
+    const [value, setValue] = useState()
+
+    const manageValue = (e) => {
+        e.preventDefault()
+        onSubmit(e,cat,value)
+        setValue("")
+    }
+
     return (
-        <ListGroup.Item key={id}>
-            <h6>{cat}</h6>
-            <Form onSubmit={onSubmit}>
-                <Form.Control type="text" style={{ width: '3rem' }} onChange={onChange} placeholder="%" />
+        <ListGroup.Item className="border d-flex flex-row align-items-bottom" key={id}>
+            <p>{cat}</p>
+            <Form className="ml-2 mr-2 align-top" onSubmit={e=>manageValue(e)}>
+                <Form.Control type="text" value={value} style={{ width: '4rem', height: '1.5rem' }} onChange={e=>setValue(e.target.value)} placeholder="€" />
             </Form>
+            <p>€</p>
         </ListGroup.Item>
     )
 }
